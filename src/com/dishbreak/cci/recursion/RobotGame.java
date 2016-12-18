bb6d4e4d51e3d438d8018aa86cd0933f483471f7
Vishal Kotcherlakota <dishbreak@gmail.com>
2016-12-17 22:34:36 -0800

    added revised robot game algorithm

--- src/com/dishbreak/cci/recursion/RobotGame.java
+++ src/com/dishbreak/cci/recursion/RobotGame.java
@@ -9,32 +9,72 @@ import java.util.*;
 
 public class RobotGame {
     
-    public static List<String> getAllPaths(int gridSize) {
+    private final int rows;
+    private final int cols;
+    private final Set<Integer> noGoX = new HashSet<>();
+    private final Set<Integer> noGoY = new HashSet<>();
+    
+    public static class Point {
+        public final int x;
+        public final int y;
+ 
+        
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+    }
+    
+    public RobotGame(int rows, int cols) {
+        this(rows, cols, null);
+    }
+    
+    public RobotGame(int rows, int cols, List<Point> noGoPoints) {
+        this.rows = rows;
+        this.cols = cols;
+        if (noGoPoints != null) {
+            for (Point point : noGoPoints) {
+                noGoX.add(point.x);
+                noGoY.add(point.y);
+            }
+        }
+    }
+    
+    public List<String> getAllPaths() {
         ArrayList<String> paths = new ArrayList<>();
-        getAllPaths(gridSize, 0, 0, new ArrayList<>(), paths);
+        getAllPaths(0, 0, new ArrayList<>(), paths);
         return paths;
     }
     
     @SuppressWarnings("unchecked")
-    private static void getAllPaths(int gridSize, int row, int col, ArrayList<String> buffer, ArrayList<String> paths) {
-        if (row == gridSize || col == gridSize) return; //off the edge of the gameboard!
-        
-        buffer.add(String.format("( %d, %d )", row, col));
-        
-        if (buffer.size() > 1) {
-            StringBuilder builder = new StringBuilder();
-            Iterator<String> iter = buffer.iterator();
-
-            while(iter.hasNext()) {
-                builder.append(iter.next());
-                if (iter.hasNext()) builder.append(" -> ");
-            }
-
-            paths.add(builder.toString());
+    private void getAllPaths(int row, int col, ArrayList<String> buffer, ArrayList<String> paths) {
+        if (row == rows || col == cols || isOccupied(row, col)) return; //off the edge of the gameboard!
+        else {
+            buffer.add(String.format("( %d, %d )", row, col));
+            if (row == rows - 1 && col == cols - 1) {
+                printPath(buffer, paths);
+            } else {
+                getAllPaths(row + 1, col, (ArrayList<String>) buffer.clone(), paths);
+                getAllPaths(row, col + 1, (ArrayList<String>) buffer.clone(), paths);
+            }       
         }
-        
-        getAllPaths(gridSize, row + 1, col, (ArrayList<String>) buffer.clone(), paths);
-        getAllPaths(gridSize, row, col + 1, (ArrayList<String>) buffer.clone(), paths);
+    }
+
+    private boolean isOccupied(int row, int col) {
+        // TODO Auto-generated method stub
+        return noGoX.contains(row) && noGoY.contains(col);
+    }
+    
+    private void printPath(ArrayList<String> buffer, ArrayList<String> paths) {
+        StringBuilder builder = new StringBuilder();
+        Iterator<String> iter = buffer.iterator();
+
+        while(iter.hasNext()) {
+            builder.append(iter.next());
+            if (iter.hasNext()) builder.append(" -> ");
+        }
+        paths.add(builder.toString());
     }
 
 }
--- test/com/dishbreak/cci/recursion/RobotGameTest.java
+++ test/com/dishbreak/cci/recursion/RobotGameTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.dishbreak.cci.recursion.RobotGame.Point;
+
 import java.util.*;
 
 public class RobotGameTest {
@@ -17,12 +19,52 @@ public class RobotGameTest {
     public void testShortPaths() {
         List<String> expected = new ArrayList<>();
         
-        expected.add("( 0, 0 ) -> ( 1, 0 )");
         expected.add("( 0, 0 ) -> ( 1, 0 ) -> ( 1, 1 )");
-        expected.add("( 0, 0 ) -> ( 0, 1 )");
         expected.add("( 0, 0 ) -> ( 0, 1 ) -> ( 1, 1 )");
         
-        assertEquals(expected, RobotGame.getAllPaths(2));
+        RobotGame game = new RobotGame(2, 2);
+        
+        assertEquals(expected, game.getAllPaths());
+    }
+    
+    @Test
+    public void testBlockedPath() {
+        List<String> expected = new ArrayList<>();
+        
+        expected.add("( 0, 0 ) -> ( 0, 1 ) -> ( 0, 2 ) -> ( 1, 2 ) -> ( 2, 2 ) -> ( 3, 2 ) -> ( 3, 3 )");
+        expected.add("( 0, 0 ) -> ( 1, 0 ) -> ( 2, 0 ) -> ( 3, 0 ) -> ( 3, 1 ) -> ( 3, 2 ) -> ( 3, 3 )");
+        expected.add("( 0, 0 ) -> ( 1, 0 ) -> ( 1, 1 ) -> ( 1, 2 ) -> ( 2, 2 ) -> ( 2, 3 ) -> ( 3, 3 )");
+        expected.add("( 0, 0 ) -> ( 0, 1 ) -> ( 0, 2 ) -> ( 1, 2 ) -> ( 2, 2 ) -> ( 2, 3 ) -> ( 3, 3 )");
+        expected.add("( 0, 0 ) -> ( 0, 1 ) -> ( 1, 1 ) -> ( 1, 2 ) -> ( 2, 2 ) -> ( 2, 3 ) -> ( 3, 3 )");
+        expected.add("( 0, 0 ) -> ( 0, 1 ) -> ( 0, 2 ) -> ( 1, 2 ) -> ( 1, 3 ) -> ( 2, 3 ) -> ( 3, 3 )");
+        expected.add("( 0, 0 ) -> ( 0, 1 ) -> ( 0, 2 ) -> ( 0, 3 ) -> ( 1, 3 ) -> ( 2, 3 ) -> ( 3, 3 )");
+        expected.add("( 0, 0 ) -> ( 1, 0 ) -> ( 1, 1 ) -> ( 1, 2 ) -> ( 2, 2 ) -> ( 3, 2 ) -> ( 3, 3 )");
+        expected.add("( 0, 0 ) -> ( 0, 1 ) -> ( 1, 1 ) -> ( 1, 2 ) -> ( 1, 3 ) -> ( 2, 3 ) -> ( 3, 3 )");
+        expected.add("( 0, 0 ) -> ( 1, 0 ) -> ( 1, 1 ) -> ( 1, 2 ) -> ( 1, 3 ) -> ( 2, 3 ) -> ( 3, 3 )");
+        expected.add("( 0, 0 ) -> ( 0, 1 ) -> ( 1, 1 ) -> ( 1, 2 ) -> ( 2, 2 ) -> ( 3, 2 ) -> ( 3, 3 )");
+        
+        Set<String> expectedSet = new HashSet<>(expected);
+        
+        List<Point> noGo = new ArrayList<>();
+        noGo.add(new Point(2, 1));
+        
+        RobotGame game = new RobotGame(4, 4, noGo);
+        
+        assertEquals(expectedSet, new HashSet<String>(game.getAllPaths()));
+
+
+    }
+    
+    @Test
+    public void testNoPath() {
+        List<Point> noGo = new ArrayList<>();
+        noGo.add(new Point(1,0));
+        noGo.add(new Point(0,1));
+        
+        RobotGame game = new RobotGame(2, 2, noGo);
+        List<String> paths = game.getAllPaths();
+        
+        assertEquals(0, paths.size());
     }
 
 }
